75e2fdd419c07e01e5ca5533e34f0e62c3ee3798
Akshit Kumar <akshitk40@gmail.com>
2021-05-16 16:05:27 +0530

    Check if Node.JS is installed or not.

--- install.bat
+++ install.bat
@@ -1,3 +1,9 @@
 @echo off
-npm install
-pause
+cls
+set nodeV=
+for /f "delims=" %%a in ('node -v') do @set nodeV=%%a
+if  %nodeV% NEQ [] (
+  npm install
+) else (
+   ECHO PLEASE INSTALL NODE.JS - https://cowin-docs.glitch.me/requirements.html#nodejs
+)
--- start.bat
+++ start.bat
@@ -1,3 +1,9 @@
 @echo off
-node index
-pause
+cls
+set nodeV=
+for /f "delims=" %%a in ('node -v') do @set nodeV=%%a
+if  %nodeV% NEQ [] (
+  node index
+) else (
+   ECHO PLEASE INSTALL NODE.JS - https://cowin-docs.glitch.me/requirements.html#nodejs
+)
