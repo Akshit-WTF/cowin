c2c3a11a24f56930dcaef2d9ebea48c37d6a98a7
Akshit Kumar <akshitk40@gmail.com>
2021-05-17 01:50:03 +0530

    Does not require Chromium installation.

--- install.bat
+++ install.bat
@@ -3,7 +3,7 @@ cls
 set nodeV=
 for /f "delims=" %%a in ('node -v') do @set nodeV=%%a
 if  %nodeV% NEQ [] (
-  npm install
+  npm install --ignore-scripts
 ) else (
    ECHO PLEASE INSTALL NODE.JS - https://cowin-docs.glitch.me/requirements.html#nodejs
 )
