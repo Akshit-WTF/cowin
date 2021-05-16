de302ed58a8242d97a6275081f94e7623880d9cc
Akshit Kumar <akshitk40@gmail.com>
2021-05-17 01:57:16 +0530

    Do not need these files.

--- fetch_IP.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set ip_address_string="IPv4 Address"
-echo Network Connection Test
-for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do echo Your IP Address is: %%f
-pause
--- fetch_IP.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Your local IP address is $(hostname -i)"
