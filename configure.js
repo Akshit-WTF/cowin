f8d75b27389bad47f2a1fb7be965a1c75a2a4e0c
Akshit Kumar <akshitk40@gmail.com>
2021-05-17 10:35:30 +0530

    Added leave blank option.

--- configure.js
+++ configure.js
@@ -13,35 +13,43 @@ let newConfig = {
 };
 
 (async () => {
-    console.log('To cancel at any time press Ctrl + C')
-    if (!originalConfig["anti-captcha-key"]) {
-        rl.question('Enter your Anti Captcha API Key: ', (answer) => {
-            newConfig["anti-captcha-key"] = answer;
-            askQuestions();
-        });
-    } else {
-        newConfig["anti-captcha-key"] = originalConfig["anti-captcha-key"];
-        askQuestions();
-    }
+    console.log('To cancel at any time press Ctrl + C. Leave blank to use previous value.');
+    askQuestions();
     function askQuestions() {
-        rl.question('Enter the phone number: ', (answer) => {
-            newConfig["phone"] = answer;
-            rl.question('Enter the beneficiary ids separated by commas: ', (answer) => {
-                newConfig.beneficiaries = answer.trim().split(',');
-                rl.question('Enter the PinCode: ', (answer) => {
-                    newConfig.pincode = answer;
-                    rl.question('Are you using Auto Token fetch? (Y/N) ', (answer) => {
-                        answer = answer.toLowerCase();
-                        newConfig.autotoken = answer === 'y';
-                        rl.question('What dose are you applying for? (1 or 2) ', (answer) => {
-                            answer = parseInt(answer);
-                            newConfig.dose = answer;
-
-                            setTimeout(function () {
-                                console.log(`Generated the config file and saved it.`);
-                                fs.writeFileSync(path.join(__dirname, './config.json'), JSON.stringify(newConfig));
-                                process.exit(0);
-                            }, 500)
+        rl.question('Enter your Anti Captcha API Key: ', (answer) => {
+            if (!answer) answer = originalConfig["anti-captcha-key"];
+            newConfig["anti-captcha-key"] = answer;
+            console.log(answer)
+            rl.question('Enter the phone number: ', (answer) => {
+                if (!answer) answer = originalConfig["phone"];
+                newConfig["phone"] = answer;
+                rl.question('Enter the beneficiary ids separated by commas: ', (answer) => {
+                    if (!answer) {
+                        newConfig.beneficiaries = originalConfig.beneficiaries
+                    } else {
+                        newConfig.beneficiaries = answer.trim().split(',');
+                    }
+                    rl.question('Enter the PinCode: ', (answer) => {
+                        if (!answer) answer = originalConfig["pincode"];
+                        newConfig.pincode = answer;
+                        rl.question('Are you using Auto Token fetch? (Y/N) ', (answer) => {
+                            if (!answer) {
+                                answer = originalConfig["autotoken"];
+                                newConfig.autotoken = answer;
+                            } else {
+                                answer = answer.toLowerCase();
+                                newConfig.autotoken = answer === 'y';
+                            }
+                            rl.question('What dose are you applying for? (1 or 2) ', (answer) => {
+                                if (!answer) answer = originalConfig["dose"];
+                                answer = parseInt(answer);
+                                newConfig.dose = answer;
+                                setTimeout(function () {
+                                    console.log(`Generated the config file and saved it.`);
+                                    fs.writeFileSync(path.join(__dirname, './config.json'), JSON.stringify(newConfig));
+                                    process.exit(0);
+                                }, 500)
+                            });
                         });
                     });
                 });
